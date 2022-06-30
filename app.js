23c11e5064895decfa9537b97e6af7888c94eed6
AKASH KUMAR <akash292984@gmail.com>
2022-06-30 10:25:57 +0530

    ejs file

--- app.js
+++ app.js
@@ -3,15 +3,43 @@ const bodyparser = require("body-parser");
 
 const app = express();
 
+app.set('view engine','ejs');
+
 app.get("/",function(req,res){
     var today  = new Date();
 
-    if(today.getDate() === 4 || today.getDate() === 0){
-        res.send("yay,it's weekend");
+    var currday = today.getDay();
+    var day = "";
+
+    switch (currday) {
+        case 0:
+            day = "sunday";
+            break;
+        case 1:
+            day = "monday";
+            break;
+        case 2:
+            day = "tuesday";
+            break;
+        case 3:
+            day = "wednesday";
+            break;
+        case 4:
+            day = "thursday";
+            break;
+        case 5:
+            day = "friday";
+            break;
+        case 6:
+            day = "saturday";
+            break;    
+    
+        default:
+            console.log("error occured" + currday);
+            break;
+        
     }
-    else{
-        res.sendFile(__dirname + "/index.html");
-    }
+    res.render("list",{kindofday : day});
 
 })
 
