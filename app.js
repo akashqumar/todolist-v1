69dcd99b804737a92bb477525377356a1d9f4a4e
AKASH KUMAR <akash292984@gmail.com>
2022-06-30 14:59:49 +0530

    css added

--- app.js
+++ app.js
@@ -3,46 +3,33 @@ const bodyparser = require("body-parser");
 
 const app = express();
 
+app.use(bodyparser.urlencoded({extended : true}));
+
+app.use(express.static("public"));
+
 app.set('view engine','ejs');
 
+let items = ["buy food","cook food","eat food"];
 app.get("/",function(req,res){
-    var today  = new Date();
+    let today  = new Date();
 
-    var currday = today.getDay();
-    var day = "";
+    let options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
 
-    switch (currday) {
-        case 0:
-            day = "sunday";
-            break;
-        case 1:
-            day = "monday";
-            break;
-        case 2:
-            day = "tuesday";
-            break;
-        case 3:
-            day = "wednesday";
-            break;
-        case 4:
-            day = "thursday";
-            break;
-        case 5:
-            day = "friday";
-            break;
-        case 6:
-            day = "saturday";
-            break;    
-    
-        default:
-            console.log("error occured" + currday);
-            break;
-        
-    }
-    res.render("list",{kindofday : day});
+    let day = today.toLocaleDateString("en-US",options);
 
-})
+    res.render("list",{kindofday : day , newlistitem : items});
 
+});
+
+app.post("/",function(req,res){
+    item = req.body.newitem;
+    items.push(item);
+    res.redirect("/");
+});
 
 app.listen(3000,function(){
     console.log("server is running at : 3000");
